e6e3352526c87859cff5a7133eed8087c193b397
jyj <tnxdls@naver.com>
2023-12-13 19:02:34 +0900

    backup weather
    added asnyc await function

--- pages/js/weatherFunc.js
+++ pages/js/weatherFunc.js
@@ -107,7 +107,10 @@ function currLocationVal() {
     const currPop = document.getElementById("currPop");
     const currSno = document.getElementById("currSno");
     const currSnoVal = document.getElementById("snoVal");
-    const promise1 = new Promise(function (resolve, reject) {
+
+    const promise1 = new Promise(async function (resolve, reject) {
+      alert("ㅈㅅ 좀만 기다려주셈");
+
       setTimeout(function () {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
@@ -146,13 +149,13 @@ function currLocationVal() {
 
               switch (skyVal) {
                 case "1": {
-                  currSky.innerHTML = "맑다";
+                  currSky.innerHTML = `<div class="currSky-result result-sunny"><span>맑아요</span><svg fill="#fdf73f" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M16,8c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S20.4,8,16,8z"></path> <path d="M16,7c0.6,0,1-0.4,1-1V3c0-0.6-0.4-1-1-1s-1,0.4-1,1v3C15,6.6,15.4,7,16,7z"></path> <path d="M8.2,9.6c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L7.5,6.1c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4L8.2,9.6 z"></path> <path d="M7,16c0-0.6-0.4-1-1-1H3c-0.6,0-1,0.4-1,1s0.4,1,1,1h3C6.6,17,7,16.6,7,16z"></path> <path d="M8.2,22.4l-2.1,2.1c-0.4,0.4-0.4,1,0,1.4c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l2.1-2.1c0.4-0.4,0.4-1,0-1.4 S8.6,22,8.2,22.4z"></path> <path d="M16,25c-0.6,0-1,0.4-1,1v3c0,0.6,0.4,1,1,1s1-0.4,1-1v-3C17,25.4,16.6,25,16,25z"></path> <path d="M23.8,22.4c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l2.1,2.1c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4 L23.8,22.4z"></path> <path d="M29,15h-3c-0.6,0-1,0.4-1,1s0.4,1,1,1h3c0.6,0,1-0.4,1-1S29.6,15,29,15z"></path> <path d="M23.1,9.9c0.3,0,0.5-0.1,0.7-0.3l2.1-2.1c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0l-2.1,2.1c-0.4,0.4-0.4,1,0,1.4 C22.6,9.8,22.8,9.9,23.1,9.9z"></path> </g> </g></svg></div>`;
                 }
                 case "3": {
-                  currSky.innerHTML = "구름이많다";
+                  currSky.innerHTML = `<div class="currSky-result result-semiCloudy"><span>구름이 많아요</span><svg fill="#5e536e" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 485.34 485.34" xml:space="preserve" stroke="#5e536e"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M89.472,275.122c-0.08-0.637-0.049-1.274-0.112-1.91c-33.377-15.01-56.732-48.514-56.732-87.418 c0-52.846,42.999-95.844,95.846-95.844c30.397,0,57.496,14.274,75.07,36.435c6.658-1.019,13.429-1.705,20.375-1.705 c6.149,0,12.203,0.557,18.21,1.385c-21.539-40.815-64.363-68.743-113.655-68.743C57.624,57.322,0,114.946,0,185.794 c0,47.779,26.271,89.471,65.064,111.602C71.932,288.776,80.072,281.144,89.472,275.122z"></path> <path d="M429.945,292.87c1.18-5.226,1.912-10.611,1.912-16.202c0-40.784-33.074-73.843-73.843-73.843 c-15.422,0-29.73,4.747-41.581,12.84c-16.537-34.49-51.699-58.356-92.516-58.356c-56.717,0-102.68,45.978-102.68,102.679 c0,11.917,2.119,23.291,5.863,33.934c-29.489,7.535-51.379,34.046-51.379,65.878c0,37.677,30.556,68.219,68.219,68.219h273.18 c37.678,0,68.219-30.542,68.219-68.219C485.34,326.516,461.475,298.892,429.945,292.87z"></path> </g> </g></svg></div>`;
                 }
                 case "4": {
-                  currSky.innerHTML = "흐립니당";
+                  currSky.innerHTML = `<div class="currSky-result result-cloudy"><span>흐려흐려</span><svg fill="#000000" viewBox="0 0 24 24" id="cloudy" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="primary" d="M17,6l-.52,0A6,6,0,0,0,6.35,8,4,4,0,1,0,6,16H17A5,5,0,0,0,17,6Z" style="fill: #000000;"></path><path id="secondary" d="M18,12h0a5,5,0,0,0-8,0h0a4,4,0,0,0,0,8h8a4,4,0,0,0,0-8Z" style="fill: #2ca9bc;"></path></g></svg></div>`;
                 }
               }
             }
@@ -211,7 +214,7 @@ function currLocationVal() {
           }
         };
         xhr.send();
-      }, 300);
+      }, 1000);
     });
 
     promise1.then(function (value) {
@@ -230,4 +233,7 @@ function currLocationVal() {
     navigator.geolocation.getCurrentPosition(success, error);
   }
 }
+
+// let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+
 window.onload = currLocationVal();
